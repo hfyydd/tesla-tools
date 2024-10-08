a40c6899d7f8ff7ce9790271e791cd4b2836c02b
hfyydd <85127658@qq.com>
2024-10-08 09:07:41 +0800

    edge runtime

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { headers } from 'next/headers';
 
+export const runtime = 'edge';
+
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto p-6 font-sans">
