7dfc325e98a6d0ae7b5a4eef3af4d399d9d698ae
hfyydd <85127658@qq.com>
2024-10-10 15:22:08 +0800

    增加属性

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { headers } from 'next/headers';
 import ClientInfo from './ClientInfo';
-
+export const runtime = 'edge';
 export default function Home() {
   return (
     <div className="max-w-4xl mx-auto p-6 font-sans">
